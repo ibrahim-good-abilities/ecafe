9d5ae21eb8f79111d235e2862550a11db1088538
ibrahim-good-abilities <hema.eldsoky19@gmail.com>
2019-09-22 13:55:51 +0200

    add

---
+++ database/migrations/2019_09_22_113742_create_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('category_name');
+            $table->string('src');
+            $table->string('title')->nullable();
+            $table->string('alt')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
