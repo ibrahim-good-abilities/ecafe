3ba8f30d8e1aebf6f46309815d5960a1ac1e7682
abduallaelnaqeb <abduallaelnaqeb>
2019-09-25 14:19:40 +0200

    fix language

--- resources/views/items/add-item.blade.php
+++ resources/views/items/add-item.blade.php
@@ -28,39 +28,39 @@
 
               <div class="row">
                  <div class="input-name col s12">
-                    <input  name="Item_Name" id="item_name" type="text" class="validate">
-                    <label for="first_name">Item Name</label>
+                    <input  name="Item_Name" id="item_name" type="text" class="validate" placeholder="{{ __('Add Item Name') }}">
+                    <label for="first_name">{{ __('Item Name') }}</label>
                  </div>
                  <div class="input-unit col s12">
-                    <input  name="Item_unit" id="item-unit" type="number" class="validate">
-                    <label  >Item Unit</label>
+                    <input  name="Item_unit" id="item-unit" type="number" class="validate" placeholder="{{ __('Add Item Unit') }}">
+                    <label  >{{ __('Item Unit') }}</label>
                  </div>
               </div>
               <div class="row">
                  <div class="item-category col s12 ">
                     <select class="icons" name="category">
-                    <option value="" disabled selected>Choose your Category</option>
+                    <option value="" disabled selected>{{ __('Choose your Category') }}</option>
                     <option value="1" data-icon="../../app-assets/images/avatar/avatar-7.png" name="category" class="circle">Category 1</option>
                     <option value="2" data-icon="../../app-assets/images/avatar/avatar-5.png" name="category" class="circle">Category 2</option>
                     <option value="3" data-icon="../../app-assets/images/avatar/avatar-3.png" name="category" class="circle">Category 3</option>
                     </select>
-                    <label>Item Category</label>
+                    <label>{{ __('Item Category') }}</label>
                  </div>
                  </div>
                  </div>
                  <div class="row">
                  <div class="input-Checkbox col s12">
-                          <label>Has Stock</label>
+                          <label>{{ __('Has Stock') }}</label>
                        <p>
                           <label>
                           <input name="has_stock" type="radio" value="1" checked/>
-                          <span>yes</span>
+                          <span>{{ __('yes') }}</span>
                           </label>
                        </p>
                        <p>
                           <label>
                           <input name="has_stock" type="radio"  value="0"/>
-                          <span>no</span>
+                          <span>{{ __('no') }}</span>
                           </label>
                        </p>
                  </div>
@@ -68,25 +68,25 @@
                  <div class="row">
                     <div class="input-alert col s12">
                           <input name="alert" id="alert-number" type="number" class="validate">
-                          <label  >Alert Number</label>
+                          <label  >{{ __('Alert Number') }}</label>
                        </div>
                  </div>
                  <div class="row">
                     <div class="input-price col s12">
                           <input   name="price" id="price" type="number" class="validate">
-                          <label  >Price</label>
+                          <label  >{{ __('Price') }}</label>
                        </div>
                  </div>
                  <div class="row">
                     <div class="input-cost col s12">
                           <input  name="cost" id="cost" type="number" class="validate">
-                          <label  >Cost</label>
+                          <label  >{{ __('Cost') }}</label>
                        </div>
                  </div>
                  <div class="row">
                  <div class="col s12 file-field input-field">
                  <div class="btn float-left">
-                    <span>Image</span>
+                    <span>{{ __('Upload Image') }}</span>
                     <input type="file" name="image">
                  </div>
                  <div class="file-path-wrapper">
@@ -97,7 +97,7 @@
 
                  <div class="row">
                        <div class="input-field col s12">
-                       <button class="btn cyan waves-effect waves-light right" type="submit" name="action">Submit
+                       <button class="btn cyan waves-effect waves-light right" type="submit" name="action">{{ __('Submit') }}
                           <i class="material-icons right">send</i>
                        </button>
                        </div>
--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -4,15 +4,15 @@
             <a class="brand-logo darken-1" href="{{ route('home') }}"><img src="{{ asset('resources/images/logo/materialize-logo-color.png') }}" alt="materialize logo" /><span class="logo-text hide-on-med-and-down">E-Café</span></a><a class="navbar-toggler" href="#"><i class="material-icons">radio_button_checked</i></a></h1>
     </div>
     <ul class="sidenav sidenav-collapsible leftside-navigation collapsible sidenav-fixed menu-shadow" id="slide-out" data-menu="menu-navigation" data-collapsible="menu-accordion">
-        <li class="bold"><a class="waves-effect waves-cyan " href="{{route('home')}}"><i class="material-icons">settings_input_svideo</i><span class="menu-title" data-i18n="">Welcome</span><span class="badge badge pill orange float-right mr-10">0</span></a>
+        <li class="bold"><a class="waves-effect waves-cyan " href="{{route('home')}}"><i class="material-icons">settings_input_svideo</i><span class="menu-title" data-i18n="">{{ __('Welcome') }}</span><span class="badge badge pill orange float-right mr-10">0</span></a>
         </li>
         <li class="bold"><a class="collapsible-header waves-effect waves-cyan " href="#"><i class="material-icons">photo_filter</i>
-                <span class="menu-title" data-i18n="">Categories</span></a>
+                <span class="menu-title" data-i18n="">{{ __('Categories') }}</span></a>
             <div class="collapsible-body">
                 <ul class="collapsible collapsible-sub" data-collapsible="accordion">
-                <li><a class="collapsible-body" href="{{route('all_categories')}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span>All Categories</span></a>
+                <li><a class="collapsible-body" href="{{route('all_categories')}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span>{{ __('All Categories') }}</span></a>
                     </li>
-                    <li><a class="collapsible-body" href="{{route('add_category')}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span>Add Category</span></a>
+                    <li><a class="collapsible-body" href="{{route('add_category')}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span>{{ __('Add new Category') }}</span></a>
                     </li>
 
                     <!--items-->
@@ -21,14 +21,14 @@
 
                 </ul>
 
-        <li class="bold"><a class="collapsible-header waves-effect waves-cyan " href="#"><i class="material-icons">photo_filter</i><span class="menu-title" data-i18n="">Items</span></a>
+        <li class="bold"><a class="collapsible-header waves-effect waves-cyan " href="#"><i class="material-icons">photo_filter</i><span class="menu-title" data-i18n="">{{ __('Items') }}</span></a>
             <div class="collapsible-body">
                 <ul class="collapsible collapsible-sub" data-collapsible="accordion">
                 <li>
-                        <a class="collapsible-body" href="{{route('items_index')}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span> All items</span></a>
+                        <a class="collapsible-body" href="{{route('items_index')}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span> {{ __('All Items') }}</span></a>
                     </li>
                     <li>
-                        <a class="collapsible-body" href="{{route('add_item')}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span>Add  new Item</span></a>
+                        <a class="collapsible-body" href="{{route('add_item')}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span>{{ __('Add  new Item') }}</span></a>
                     </li>
 
                 </ul>
