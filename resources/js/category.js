a4a894795a9733cbc11add168038779f0dfd3ff4
abduallaelnaqeb <abduallaelnaqeb>
2019-10-01 16:22:56 +0200

    add category file

---
+++ resources/js/category.js
@@ -0,0 +1,24 @@
+/*
+ * DataTables - Tables
+ */
+
+
+$(function () {
+
+    var language = "https://cdn.datatables.net/plug-ins/1.10.19/i18n/Arabic.json";
+
+
+  $('#category-table').DataTable({
+    "responsive": true,
+   
+    "language": {
+        "url": language
+    } ,
+    columnDefs: [
+      { orderable: false, targets: -1 }
+   ]
+   });
+
+
+  
+})
