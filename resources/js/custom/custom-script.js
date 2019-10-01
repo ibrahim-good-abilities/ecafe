a3cfc8c0ccb3c1a1eb0d2a016493fc3a3ea2f12e
Hussam Hussien <7@>
2019-10-01 12:30:44 +0200

    add confirmation before delete

--- resources/js/custom/custom-script.js
+++ resources/js/custom/custom-script.js
@@ -9,3 +9,26 @@ NOTE:
 ------
 PLACE HERE YOUR OWN JS CODES AND IF NEEDED.
 WE WILL RELEASE FUTURE UPDATES SO IN ORDER TO NOT OVERWRITE YOUR CUSTOM SCRIPT IT'S BETTER LIKE THIS. */
+$(document).ready(function() {
+
+    $(document).on('click', '.delete-with-confirmation', function(e) {
+        e.preventDefault();
+        var href = $(this).attr('href');
+        swal({
+            title: "Are you sure?",
+            icon: 'warning',
+            buttons: {
+                cancel: true,
+                delete: 'Yes, Delete It'
+            }
+        }).then((willDelete) => {
+            if (willDelete) {
+                $(this).removeClass('delete-with-confirmation');
+                window.location.href = href; //causes the browser to refresh and load the requested url
+            }
+        });
+
+    });
+
+
+});
