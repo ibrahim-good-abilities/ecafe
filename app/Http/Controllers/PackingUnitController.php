9c13ced3206a343ad9e28be558875bf17a4c9bee
Hussam Hussien <7@>
2019-10-06 13:48:20 +0200

    add packing units listing

--- app/Http/Controllers/PackingUnitController.php
+++ app/Http/Controllers/PackingUnitController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\PackingUnit;
+
 class PackingUnitController extends Controller
 {
     /**
@@ -14,7 +16,7 @@ class PackingUnitController extends Controller
     public function index()
     {
         $units = PackingUnit::all();
-        return view('packing_units.index')->with('units',$categories);
+        return view('packing_units.index')->with('units',$units);
     }
 
     /**
