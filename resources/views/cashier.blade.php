55f7fcfe497317aa3ca35b97b3df38dd7ca9b32b
abduallaelnaqeb <abduallaelnaqeb>
2019-10-09 15:44:05 +0200

    fix cashier screen

--- resources/views/cashier.blade.php
+++ resources/views/cashier.blade.php
@@ -1,8 +1,7 @@
 @extends('parista-layout')
 @section('title', __('All Orders'))
 @section('page_css')
-<link rel="stylesheet" type="text/css" href="{{asset('resources/vendors/slick/slick.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('resources/vendors/slick/slick-theme.css')}}">
+
 <link rel="stylesheet" type="text/css" href="{{asset('resources/css/cashier.css')}}">
 @endsection
 @section('middle_content')
@@ -19,57 +18,64 @@
     </button>
 </div>
 @endif
-<div class="col s12">
+
+    
 
 
                     <div class="empty-orders">
                         <h4>لا يوجد لديك طلبات الان </h4>
                     </div>
-    <div id="orders">
+ 
 
             <input type="hidden" id ="_order_token" value="{{ csrf_token()}}"/>
-            @foreach($orders as $order)
-                <div>
-                    <div class="order-box">
-                        <ul id="issues-collection" class="collection z-depth-1">
-                            <li class="collection-item avatar">
-                                <i class="material-icons green accent-2 circle">attach_file</i>
-                                <h6 class="collection-header m-0">Order #{{$order->id}}</h6>
-                                <p>Table @ {{$order->table_number}}</p>
-                            </li>
-                            @php
-                                $order_items =  App\Http\Controllers\OrderController::getOrderItems($order->id);
-                            @endphp
-                            @foreach($order_items as $item)
-                            <li class="collection-item">
-                                <div class="row">
-                                    <div class="col s8">
-                                        <p class="collections-title font-weight-600">{{ $item->name}} X {{ $item->quantity}}</p>
-                                    </div>
+           
+<div class="container">
+    <div class="row">
+          @foreach($orders as $order)
+        <div class="col s4 order-content">
+                                                    
+                        <div class="order-box">
+                            <ul id="issues-collection" class="collection z-depth-1">
+                                <li class="collection-item avatar">
+                                    <i class="material-icons green accent-2 circle">attach_file</i>
+                                    <h6 class="collection-header m-0">Order #{{$order->id}}</h6>
+                                    <p>Table @ {{$order->table_number}}</p>
+                                </li>
+                                @php
+                                    $order_items =  App\Http\Controllers\OrderController::getOrderItems($order->id);
+                                @endphp
+                                @foreach($order_items as $item)
+                                <li class="collection-item">
+                                    <div class="row">
+                                        <div class="col s8">
+                                            <p class="collections-title font-weight-600">{{ $item->name}} X {{ $item->quantity}}</p>
+                                        </div>
 
-                                </div>
-                            </li>
-                            @endforeach
-                            <li class="collection-item">
-                                <div class="row">
-                                    <div class="col s12">
-                                        <p class="right-align">{{$order->notes}}</p>
                                     </div>
+                                </li>
+                                @endforeach
+                                <li class="collection-item">
+                                    <div class="row">
+                                        <div class="col s12">
+                                            <p class="right-align">{{$order->notes}}</p>
+                                        </div>
 
-                                    <div class="col s12 center-align">
-                                        <a class="modal-trigger" href="#payment">
-                                            <button class="waves-effect waves-light red btn btn-small ">{{__('Payment')}}</button>
-                                        </a>
+                                        <div class="col s12 center-align">
+                                            <a class="modal-trigger" href="#payment">
+                                                <button class="waves-effect waves-light red btn btn-small ">{{__('Payment')}}</button>
+                                            </a>
+                                        </div>
                                     </div>
-                                </div>
-                            </li>
-                        </ul>
+                                </li>
+                            </ul>
+                        </div> 
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+ 
+               
+    <div>
+</div>     
 
-    </div>
-</div>
 <div id="payment" class="modal">
     <form action="{{ route('main_stock_operations') }}" method="post">
             @csrf
@@ -108,7 +114,7 @@
 
 
 @section('page_js')
-<script src="{{asset('resources/vendors/slick/slick.min.js')}}" type="text/javascript"></script>
+
 <script src="https://js.pusher.com/5.0/pusher.min.js" type="text/javascript"></script>
 <script src="{{asset('resources/js/cashier.js')}}" type="text/javascript"></script>
 @endsection
