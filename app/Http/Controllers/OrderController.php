bb1b419179bdd4be83ce9fc51a371d8f4708fad7
KarimMostafa <33594495+kareemegy@users.noreply.github.com>
2019-10-13 16:29:18 +0200

    create the login system

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -14,6 +14,11 @@ use App\Events\NewNotification;
 
 class OrderController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
