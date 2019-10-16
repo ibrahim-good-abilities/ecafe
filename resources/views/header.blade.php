bec05f3073198691e34e4bbbbd60a272527da54f
abduallaelnaqeb <abduallaelnaqeb>
2019-10-16 15:07:01 +0200

    add Show settings + logout buttons on the top menu

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -12,12 +12,12 @@
                     <li class="hide-on-large-only"><a class="waves-effect waves-block waves-light search-button" href="javascript:void(0);"><i class="material-icons">search</i></a></li>
 
                     <li><a class="waves-effect waves-block waves-light notification-button" href="javascript:void(0);" data-target="notifications-dropdown"><i class="material-icons">notifications_none<small class="notification-badge">{{$count}}</small></i></a></li>
-                   {{--
-                   @if((Route::currentRouteName()!=='welcome' ) && (Route::currentRouteName()!=='parista'))
-                    <li><a class="waves-effect waves-block waves-light profile-button" href="javascript:void(0);" data-target="profile-dropdown"><span class="avatar-status avatar-online"><img src="{{asset('resources/images/avatar/default_avatar.png')}}" alt="avatar"><i></i></span></a></li>
+                
+                  
+                    <li><a class="waves-effect waves-block waves-light profile-button" href="javascript:void(0);" data-target="profile-dropdown"><i class="material-icons">person</i></span></a></li>
                     <!-- <li><a class="waves-effect waves-block waves-light sidenav-trigger" href="#" data-target="slide-out-right"><i class="material-icons">format_indent_increase</i></a></li> -->
-                    @endif
-                    --}}
+                    
+                
 
                 </ul>
                 <!-- notifications-dropdown-->
@@ -36,12 +36,10 @@
                 </ul>
                 <!-- profile-dropdown-->
                 <ul class="dropdown-content" id="profile-dropdown">
-                    <li><a class="grey-text text-darken-1" href="user-profile-page.html"><i class="material-icons">person_outline</i> Profile</a></li>
-                    <li><a class="grey-text text-darken-1" href="app-chat.html"><i class="material-icons">chat_bubble_outline</i> Chat</a></li>
-                    <li><a class="grey-text text-darken-1" href="page-faq.html"><i class="material-icons">help_outline</i> Help</a></li>
-                    <li class="divider"></li>
-                    <li><a class="grey-text text-darken-1" href="user-lock-screen.html"><i class="material-icons">lock_outline</i> Lock</a></li>
-                    <li><a class="grey-text text-darken-1" href="user-login.html"><i class="material-icons">keyboard_tab</i> Logout</a></li>
+                    @if(auth()->user() && auth()->user()->role->role_name == 'admin')
+                    <li><a class="grey-text text-darken-1" href="{{ route('settings') }}"><i class="material-icons">settings</i>Settings</a></li>
+                    @endif
+                    <li><a class="grey-text text-darken-1" href="{{ route('logout') }}"><i class="material-icons">keyboard_tab</i> Logout</a></li>
                 </ul>
             </div>
             <nav class="display-none search-sm">
