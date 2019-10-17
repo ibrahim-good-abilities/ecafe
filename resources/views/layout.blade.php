309ab99721b6eb05e0197783c24191642f535730
abduallaelnaqeb <abduallaelnaqeb>
2019-10-17 12:21:32 +0200

    fix title page

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html class="loading" lang="en" data-textdirection="ltr">
 <!-- BEGIN: Head-->
-
+@php
+$settings =  App\Http\Controllers\SettingController::getAll();
+@endphp
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,7 +11,7 @@
     <meta name="description" content="Materialize is a Material Design Admin Template,It's modern, responsive and based on Material Design by Google.">
     <meta name="keywords" content="materialize, admin template, dashboard template, flat admin template, responsive admin template, eCommerce dashboard, analytic dashboard">
     <meta name="author" content="ThemeSelect">
-    <title>@yield('title')</title>
+    <title>{{ $settings['company_name'] }}</title>
     <link rel="apple-touch-icon" href="{{asset('resources/images/favicon/apple-icon-152x152.png')}}">
     <link rel="shortcut icon" type="image/x-icon" href="{{asset('resources/images/favicon/favicon-32x32.png')}}">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
